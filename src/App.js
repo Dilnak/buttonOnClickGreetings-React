48b54bef3dd676eeafc906a4e3a051a9b5ab8680
Dilna <dilnapilassery95@gmail.com>
2019-03-19 11:28:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component{
+   constructor(){
+      super();
+      this.state={
+         data:" "
+      }
+      this.displayMessage=this.displayMessage.bind(this)
+   };
+   displayMessage(){
+      this.setState({data:"Good morning Dilna"})
+   }
+   render()
+   {
+      var myStyles={
+         color:"red",
+      fontSize:50,
+      
+      }
+      return(
+         <div>
+            <button onClick={this.displayMessage}>click here for greeting message</button>
+            <h1 style={myStyles}>{this.state.data}</h1>
+         </div>
+      );
+   }
 }
-
 export default App;
